43718f66cbeb5682e0c1ea776f31ae2547a8c967
Hafiza Maham <hafiza.maham032@gmail.com>
2026-03-14 01:17:50 +0500

    Testing CI/CD

--- webapp.py
+++ webapp.py
@@ -5,9 +5,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    # Yeh line batayegi ke kaunsa container request handle kar raha hai
+    # This line tells us which container is handling the request, useful for debugging in a scaled environment
     container_id = os.uname()[1] if hasattr(os, 'uname') else "Windows-Machine"
-    return f"<h1>Auto-Scale Sentinel: Active</h1><p>Handled by Container: {container_id}</p>"
+    return f"<h1>Auto-Scale Sentinel: Active <br> Sentinel Protected Website v2.0</h1><p>Handled by Container: {container_id}</p>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
