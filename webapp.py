0b07165bc9561feb97e7cc0260b61ff5fb982572
Hafiza Maham <hafiza.maham032@gmail.com>
2026-03-16 14:01:50 +0500

    Updated webapp

--- webapp.py
+++ webapp.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def home():
     # This line tells us which container is handling the request, useful for debugging in a scaled environment
     container_id = os.uname()[1] if hasattr(os, 'uname') else "Windows-Machine"
-    return f"<h1>Auto-Scale Sentinel: Active <p>Handled by Container: {container_id}</p>"
+    return f"<h1>Auto-Scale Sentinel: Active </h1><p>Handled by Container: {container_id}</p>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
